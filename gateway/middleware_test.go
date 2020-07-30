b2bf34ab77f88808cd1dbf31712d6638439873f8
prakriti07 <57622944+prakriti07@users.noreply.github.com>
2020-07-30 17:05:00 +0530

    Update middleware_test.go

--- gateway/middleware_test.go
+++ gateway/middleware_test.go
@@ -4,12 +4,12 @@ import (
 	"net/http"
 	"testing"
 
-	"github.com/TykTechnologies/tyk/apidef"
+	"github.com/prakriti07/tyk/apidef"
 	cache "github.com/pmylund/go-cache"
 	"github.com/stretchr/testify/assert"
 
-	"github.com/TykTechnologies/tyk/config"
-	"github.com/TykTechnologies/tyk/user"
+	"github.com/prakriti07/tyk/config"
+	"github.com/prakriti07/tyk/user"
 )
 
 type mockStore struct {
