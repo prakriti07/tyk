c84a1001ff8bf7b6b3997b90197d2bf6526264af
prakriti07 <57622944+prakriti07@users.noreply.github.com>
2020-07-30 17:12:06 +0530

    Update mw_http_signature_validation.go

--- gateway/mw_http_signature_validation.go
+++ gateway/mw_http_signature_validation.go
@@ -20,9 +20,9 @@ import (
 
 	"github.com/sirupsen/logrus"
 
-	"github.com/TykTechnologies/tyk/apidef"
-	"github.com/TykTechnologies/tyk/regexp"
-	"github.com/TykTechnologies/tyk/user"
+	"github.com/prakriti07/tyk/apidef"
+	"github.com/prakriti07/tyk/regexp"
+	"github.com/prakriti07/tyk/user"
 )
 
 const dateHeaderSpec = "Date"
