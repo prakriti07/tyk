85bfd3480b404f77abe6817bfc918c64d8f2ab88
prakriti07 <57622944+prakriti07@users.noreply.github.com>
2020-07-30 17:19:36 +0530

    Update mw_redis_cache_test.go

--- gateway/mw_redis_cache_test.go
+++ gateway/mw_redis_cache_test.go
@@ -8,10 +8,10 @@ import (
 	"strings"
 	"testing"
 
-	"github.com/TykTechnologies/tyk/apidef"
-	"github.com/TykTechnologies/tyk/config"
-	"github.com/TykTechnologies/tyk/storage"
-	"github.com/TykTechnologies/tyk/test"
+	"github.com/prakriti07/tyk/apidef"
+	"github.com/prakriti07/tyk/config"
+	"github.com/prakriti07/tyk/storage"
+	"github.com/prakriti07/tyk/test"
 )
 
 func TestRedisCacheMiddleware_WithCompressedResponse(t *testing.T) {
