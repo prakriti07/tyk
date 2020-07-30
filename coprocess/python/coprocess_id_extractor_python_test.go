cd7be7dce439edba1794eb21cc46ea74e8a150bb
prakriti07 <57622944+prakriti07@users.noreply.github.com>
2020-07-30 11:12:06 +0530

    Update coprocess_id_extractor_python_test.go

--- coprocess/python/coprocess_id_extractor_python_test.go
+++ coprocess/python/coprocess_id_extractor_python_test.go
@@ -6,9 +6,9 @@ import (
 	"testing"
 	"time"
 
-	"github.com/TykTechnologies/tyk/config"
-	"github.com/TykTechnologies/tyk/gateway"
-	"github.com/TykTechnologies/tyk/test"
+	"github.com/prakriti07/tyk/config"
+	"github.com/prakriti07/tyk/gateway"
+	"github.com/prakriti07/tyk/test"
 )
 
 var pythonIDExtractorHeaderValue = map[string]string{
